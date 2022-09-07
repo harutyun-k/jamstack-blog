85cc42352625996d0f4d190f60a61e4fd0e489d7
Harutyun Karapetyan <harutyun.k@ramotion.agency>
2022-09-07 10:18:36 +0300

    Update styles in post-archive

--- src/templates/posts-archive.tsx
+++ src/templates/posts-archive.tsx
@@ -7,17 +7,24 @@ const PostArchive = ({pageContext, data}) => {
   const allArticles = data.allDatoCmsArticle.nodes
 
   return (
-    <section className="m-5 mb-20">
-      <Link to="/">
+    <section className="container m-auto flex flex-col gap-4 p-3 md:p-5 lg:p-8">
+      <Link
+        className="mb-3 p-2 underline uppercase"
+        to="/"
+      >
         Home
       </Link>
-      <h2 className="mb-5 font-black text-2xl uppercase">Archive</h2>
-      {allArticles.map(article => {
-        return <Article
-          key={article.id}
-          data={article} 
-        />
-      })}
+      <h1 className="mb-15 font-black text-3xl uppercase">
+        Archive
+      </h1>
+      <div className="mb-10 grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {allArticles.map(article => {
+          return <Article
+            key={article.id}
+            data={article} 
+          />
+        })}
+      </div>
       <Pagination data={pageContext} />
     </section>
   )
