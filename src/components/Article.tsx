886a278af31611d7b829185e2bd08fc0c7411576
Harutyun Karapetyan <harutyun.k@ramotion.agency>
2022-09-07 08:58:55 +0300

    Add styles, interface in Article

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,35 +2,47 @@ import * as React from "react"
 import { Link } from "gatsby"
 import Title from "../components/Title"
 
-const Article = ({data}) => {
-  const dataID = data.id
-  const slug = data.slug
-  const coverURL = data.cover.url
-  const category = data.tag[0].category
-  const title = data.title
-  const time = data.meta.createdAt
+interface IArticle {
+  cover: {
+    url: String;
+  }
+  id: String;
+  meta: {
+    createdAt: String;
+  }
+  slug: String;
+  subtitle: String;
+  tag: any[];
+  title: String;
+}
 
+interface ArticleProps {
+  data: IArticle;
+}
+
+export default function Article({data}: ArticleProps) {
   return (
-    <article className="mb-5">
-      <Link to={`/${slug}`}>
+    <article className="relative mb-5 max-w-lg bg-slate-100 rounded-2xl overflow-hidden">
+      <Link to={`/${data.slug}`}>
         <img
-          src={coverURL}
-          width="300"
+          className="w-full h-60 object-cover"
+          src={data.cover.url}
+          alt={data.title}
         />
       </Link>
-      <span>
-        {category}
-      </span>
-      <Title
-        tagName="h3"
-        text={title}
-        key={dataID}
-      />
-      <time>
-        { time }
-      </time>
+      <div className="p-8 flex flex-col gap-2">
+        <span className="block font-bold">
+          { data.tag[0].category }
+        </span>
+        <Title
+          tagName="h3"
+          text={data.title}
+          key={data.id}
+        />
+        <time className="italic text-sm">
+          { data.meta.createdAt }
+        </time>
+      </div>
     </article>
   )
-};
-
-export default Article;
+}
