fb139ddf78d4e4d48347fb40cfff08af82c24844
Harutyun Karapetyan <harutyun.k@ramotion.agency>
2022-09-08 10:50:51 +0300

    Update article-post

--- src/templates/article-post.tsx
+++ src/templates/article-post.tsx
@@ -47,23 +47,23 @@ export default function ArticlePost ({data}: IArticlePost) {
   const content = result.content
 
   return (
-    <div className="container m-auto flex flex-col p-3 md:p-5 lg:p-8">
+    <div className="container m-auto flex flex-col gap-4 p-3 md:p-5 lg:p-8">
       <Link to="/">
         Home
       </Link>
       <Link
-        className="uppercase underline"
+        className="font-black uppercase"
         to={`/category/${tag.toLowerCase()}`}
       >
-        {tag}
+        { tag }
       </Link>
-      <time>
+      <time className="italic">
         {time}
       </time>
-      <h1 className="font-bold">
+      <h1 className="mb-3 font-black text-5xl uppercase">
         {title}
       </h1>
-      <h2 className="font-bold">
+      <h2 className="font-black text-4xl uppercase">
         {subtitle}
       </h2>
       <div
@@ -71,8 +71,10 @@ export default function ArticlePost ({data}: IArticlePost) {
         dangerouslySetInnerHTML={{__html: content}}
       />
 
-      <section>
-        <h2 className="mb-5 font-black text-2xl uppercase">Latest news</h2>
+      <section className="flex flex-col gap-5">
+        <h2 className="w-full font-black text-3xl uppercase mb-5">
+          Latest news
+        </h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
           {latestNews.map(news => {
             return <Article
@@ -82,7 +84,7 @@ export default function ArticlePost ({data}: IArticlePost) {
           })}
         </div>
         <Link
-          className="font-black text-2xl underline uppercase"
+          className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg p-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 text-center uppercase"
           to="/archive"
         >
           Archive
