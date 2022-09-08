14bc5ebba57cb0d9e78237cb00fe3602f65dab20
Harutyun Karapetyan <harutyun.k@ramotion.agency>
2022-09-08 08:16:28 +0300

    Update Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,43 +1,62 @@
 import { Link } from "gatsby";
 import * as React from "react"
 
-const Pagination = ({data}) => {
-  const { numPages, currentPage } = data;
-  if (!numPages || !currentPage) return null;
+interface IPagination {
+  data: {
+    limit: Number;
+    skip: Number;
+    numPages: Number;
+    currentPage: Number;
+  }
+}
 
-  const nums = []
+export default function Pagination({data}: IPagination): JSX.Element {
+  const { numPages, currentPage } = data;
+  if (!numPages || !currentPage) {
+    return null;
+  }
+
+  const nums: number[] = []
 
   for (let i = 0; i < numPages; i++) {
     nums.push(i)
   }
 
-  const isFirst = currentPage === 1
-  const prev =
+  const isFirst: boolean = currentPage === 1
+  const prev: string =
     currentPage === 2 ? "/archive" : `/archive/page/${currentPage - 1}`
-  const isLast = currentPage === numPages
-  const nextPage = currentPage + 1
+  const isLast: boolean = currentPage === numPages
+  const nextPage: number = currentPage + 1
 
   return (
-    <div className="pagination flex flex-wrap gap-4">
+    <div className="flex flex-wrap items-center gap-4">
       {!isFirst && (
-        <span>
-          <Link to={prev} rel="prev">
-            Previous page
-          </Link>
-        </span>
+        <Link
+          className="flex w-10 h-10 block bg-slate-200 hover:bg-slate-300 rounded-full"
+          to={prev}
+          rel="prev"
+          aria-label="Previous page"
+        >
+          <svg className="m-auto w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+          </svg>
+        </Link>
       )}
       <div>
         {`${currentPage} of ${nums.length}`}
       </div>
       {!isLast && (
-        <Link to={`/archive/page/${nextPage}`} role="next">
-          <span className="visually-hidden">
-            Next page
-          </span>
+        <Link
+          className="flex w-10 h-10 block bg-slate-200 hover:bg-slate-300 rounded-full"
+          to={`/archive/page/${nextPage}`}
+          role="next"
+          aria-label="Next page"
+        >
+          <svg className="m-auto w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+          </svg>
         </Link>
       )}
     </div>
   )
-};
-
-export default Pagination
+}
