dac7390c057849e34486297fab6eee2176f84313
Harutyun Karapetyan <harutyun.k@ramotion.agency>
2022-09-08 08:55:09 +0300

    Update styles in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ const IndexPage = ({data}) => {
           />
         })}
         <Link
-          className="w-full font-black underline"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg p-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 text-center uppercase"
           style={{ gridColumn: "1 / -1" }}
           to="/archive"
         >
